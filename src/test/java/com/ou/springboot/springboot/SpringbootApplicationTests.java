4a597a91d1d39a2952a865bc9c7fe3fd9142b0ae
baicha <1293382906@qq.com>
2020-02-23 17:02:51 +0800

    first commit

---
+++ src/test/java/com/ou/springboot/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou.springboot.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
